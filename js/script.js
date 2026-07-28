ee6ed6980d2e099ed0594999e24a5f96d66a5fd9
kvnginco <kvnginco@gmail.com>
2026-07-28 19:20:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const dropdowns = document.querySelectorAll(".dropdown");
 
+dropdowns.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+const menu=button.nextElementSibling;
+
+if(menu.style.display==="block"){
+
+menu.style.display="none";
+
+}else{
+
+menu.style.display="block";
+
+}
+
+});
+
+});
